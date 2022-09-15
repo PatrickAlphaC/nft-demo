30f80541eed1b5a1c696c14a8ca3a9c3157f9212
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-09-15 16:55:10 -0400

    updated for goerli

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -11,7 +11,7 @@ def deploy_and_create():
     account = get_account()
     # We want to be able to use the deployed contracts if we are on a testnet
     # Otherwise, we want to deploy some mocks and use those
-    # Rinkeby
+    # Goerli
     advanced_collectible = AdvancedCollectible.deploy(
         get_contract("vrf_coordinator"),
         get_contract("link_token"),
